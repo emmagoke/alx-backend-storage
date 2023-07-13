fa8e18f283113e63c591d5e193eeaba6db8d2150
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-13 18:35:41 +0100

    created table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql0-uniq_users.sql
@@ -1,10 +1,7 @@
 -- This SQL query creates a table users
--- With these attributes:
--- 	id, integer, never null, auto increment and primary key
--- 	email, string (255 characters), never null and unique
--- 	name, string (255 characters)
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+-- 	id, integer, email,  name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
