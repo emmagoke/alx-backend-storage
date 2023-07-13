f3a01a9c310318799a50f3e282bc184d7ac027c6
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-13 19:12:42 +0100

    created table users with enum

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,9 +5,11 @@
 --    name, string (255 characters)
 --    country, enumeration of countries: US, CO and TN, never null
 --    (= default will be the first element of the enumeration, here US)
+CREATE TYPE countries ENUM('US', 'CO', 'TN');
+
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL
+    country countries DEFAULT 'US'
 )
