612724719e65bb096649863056b2abce844f46ec
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-13 18:28:37 +0100

    created table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql0-uniq_users.sql
@@ -3,8 +3,8 @@
 -- 	id, integer, never null, auto increment and primary key
 -- 	email, string (255 characters), never null and unique
 -- 	name, string (255 characters)
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );
