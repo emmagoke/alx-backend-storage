492bf1ffdb176fbc81be28ef8c10d5b6a4bd4eff
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-14 16:47:33 +0100

    created the trigger valid_email_change

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER `valid_email_change`
 AFTER UPDATE ON `users` FOR EACH ROW
 BEGIN
   IF
-    NEW.email != OLD.email SET NEW.valid_email
+    NEW.email != OLD.email SET NEW.valid_email = 0
   END IF
 END $$$
 DELIMITER;
