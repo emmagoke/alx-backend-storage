ad11433f0a3d885f9dfb75a14ced1a88b7f18971
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-14 16:58:36 +0100

    created the trigger valid_email_change

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- This query creates a trigger that resets the attribute valid_email
 -- only when the email has been changed.
+DROP TRIGGER IF EXISTS `valid_email_change`;
 DELIMITER $$$
 CREATE TRIGGER `valid_email_change`
 BEFORE UPDATE ON `users` FOR EACH ROW
