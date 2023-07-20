6ccf3d77085d0e02171921119b7fdec84646d876
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-20 03:34:56 +0100

    created the get, get_str, get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ operation as regards to Redis.
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -27,3 +27,31 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Callable) -> Any:
+    	"""
+    	This method take a key string argument and an optional Callable
+    	This callable will be used to convert the data back to
+    	the desired format.
+    	"""
+
+    	value = self._redis.get(key)
+    	if fn is None:
+    		return value
+    	return fn(value)
+
+    def get_str(self, key: str) -> str:
+    	"""
+    	This method return a return a utf-8 decode value of
+    	self.get
+    	"""
+
+    	return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+    	"""
+    	This method return a integer fashion of the value
+    	self.get returns
+    	"""
+
+    	return self.get(key, int)
